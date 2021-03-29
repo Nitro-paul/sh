2ecf4739dd2adcd1a353bff63a3eb777955cdc03
Nitro-paul <76756239+Nitro-paul@users.noreply.github.com>
2021-03-29 14:18:13 +0200

    Add files via upload

---
+++ dc.sh
@@ -0,0 +1,2 @@
+sudo wget -o dc https://dl.discordapp.net/apps/linux/0.0.14/discord-0.0.14.deb
+sudo dpkg -i discord-0.0.14.deb
---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo setxkbmap de
+sudo apt install python -y
+sudo apt install pip -y
+sudo apt install htop -y
+sudo apt install gnome-disk-utility -y
+sudo bash dc.sh
